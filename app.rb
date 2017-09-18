8009639343606e79f3e9560fc0a23746d531fc1c
mswelcome <mattwelcome@gmail.com>
2017-09-18 16:03:39 -0400

    Fingers crossed

--- app.rb
+++ app.rb
@@ -20,14 +20,17 @@ post '/records' do
 begin
 
   wbinfo = {
+
     host: ENV['RDS_HOST'],
     port: ENV['RDS_PORT'],
     dbname: ENV['RDS_DB_NAME'],
     user: ENV['RDS_USERNAME'],
     password: ENV['RDS_PASSWORD']
+
   }
 
   wb = PG::Connection.new(wbinfo)
+
   wb.exec "create table pb (
              id int primary key,
              first varchar(50),
@@ -36,8 +39,9 @@ begin
              city varchar(25),
              state varchar(2),
              zip varchar(5),
-             phone varchar(10)"
-  wb.exec INSERT INTO public.pb id, "first", "last", street, city, state, zip)VALUES('session[:r][0]', 'session[:r][1]', 'session[:r][2]', 'session[:r][3]', 'session[:r][4]', 'session[:r][5]', 'session[:r][6]');
+             phone varchar(10))"
+
+  wb.exec INSERT INTO public.pb (id, "first", "last", "street", "city", "state", "zip", "phone")VALUES('session[:r][0]', 'session[:r][1]', 'session[:r][2]', 'session[:r][3]', 'session[:r][4]', 'session[:r][5]', 'session[:r][6]');
 
 
 rescue PG::Error => e
@@ -58,20 +62,25 @@ get '/get' do
 begin
 
   wbinfo = {
+
     host: ENV['RDS_HOST'],
     port:ENV['RDS_PORT'],
     dbname:ENV['RDS_DB_NAME'],
     user:ENV['RDS_USERNAME'],
     password:ENV['RDS_PASSWORD']
+
   }
 
   wb = PG::Connection.new(wbinfo)
   rs = wb.exec 'SELECT * FROM pb LIMIT 7'
 
   list = []
+
   rs.each do |row|
-    list << "%s %s %s" % [row['First'], row['Last'], row['Street'], row['City'], row['State'], row['Zip'], row['Phone']]
+     list << [row['First'], row['Last'], row['Street'], row['City'], row['State'], row['Zip'], row['Phone']]
   end
+
+  #{}"%s %s %s %s %s %s %s" %
 
   rescue PG::Error => e
 
@@ -85,6 +94,6 @@ begin
 end
 
 
-erb :showget, locals:{list: list}
+  erb :showget, locals:{list: list}
 
 end
